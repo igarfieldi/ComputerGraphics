54168810385dd97cb1c2c1b1cd2ac63b5200da45
Florian Bethe <garfieldthe3rd@gmail.com>
2017-06-26 22:08:34 +0200

    Update brazil_flag.cpp
    Fixed discrepancies between float and double; added usage of M_PI

--- ΠαναγιώταΘωμοπούλου_Π14053_ΔημήτρηςΤζιλιβάκης_Π14193/Thema01/brazil_flag.cpp
+++ ΠαναγιώταΘωμοπούλου_Π14053_ΔημήτρηςΤζιλιβάκης_Π14193/Thema01/brazil_flag.cpp
@@ -1,10 +1,11 @@
 #include <GL/glut.h>
+
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <cmath>
 
 const int W_HEIGHT = 720;
 const int W_WIDTH = 504;
-const float PI = 3.14;
 
 void eq_triangle( int base, int height,int x,int y, int i){
     // Each set of 3 vertices form a triangle
@@ -12,8 +13,8 @@ void eq_triangle( int base, int height,int x,int y, int i){
 	glBegin (GL_POLYGON);
 	for( int n = 0; n<3; n++){
 	
-		float xn1 = x+base * cos(2*PI*n/5);
-		float yn1 = y+base * sin(2*PI*n/5);
+		float xn1 = x+base * cosf(2.0f*M_PI*n/5.0f);
+		float yn1 = y+base * sinf(2.0f*M_PI*n/5.0f);
 		glVertex2f(xn1 , yn1);
 		if(n==2){
 			glVertex2f(xn1+height , yn1+height);
@@ -27,21 +28,20 @@ void eq_triangle( int base, int height,int x,int y, int i){
 
 }
 void DrawStar(float fX, float fY) {
-	const float kfPi = 3.1415926535897932384626433832795;
-	const float kfRadius = 0.0616/2.0;
-	const float kfInnerRadius = kfRadius*(1.0/(sin((2.0*kfPi)/5.0)*2.0*cos(kfPi/10.0) + sin((3.0*kfPi)/10.0)));
-	glColor3f(1.0, 1.0, 1.0);
+	const float kfRadius = 0.0616f/2.0f;
+	const float kfInnerRadius = kfRadius*(1.0f/(sinf((2.0f*M_PI)/5.0f)*2.0f*cosf(M_PI/10.0f) + sinf((3.0f*M_PI)/10.0f)));
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_TRIANGLE_FAN);
-		glVertex3f(fX, fY, 0.0);
+		glVertex3f(fX, fY, 0.0f);
 		for (int iVertIndex = 0; iVertIndex < 10; ++iVertIndex) {
-			float fAngleStart	= kfPi/2.0 + (iVertIndex*2.0*kfPi)/10.0;
-			float fAngleEnd		= fAngleStart + kfPi/5.0;
+			float fAngleStart	= M_PI/2.0f + (iVertIndex*2.0f*M_PI)/10.0f;
+			float fAngleEnd		= fAngleStart + M_PI/5.0f;
 			if (iVertIndex % 2 == 0) {
-				glVertex3f(fX + kfRadius*cos(fAngleStart)/1.9, fY + kfRadius*sin(fAngleStart), 0.0);
-				glVertex3f(fX + kfInnerRadius*cos(fAngleEnd)/1.9, fY + kfInnerRadius*sin(fAngleEnd), 0.0);
+				glVertex3f(fX + kfRadius*cosf(fAngleStart)/1.9f, fY + kfRadius*sinf(fAngleStart), 0.0f);
+				glVertex3f(fX + kfInnerRadius*cosf(fAngleEnd)/1.9f, fY + kfInnerRadius*sinf(fAngleEnd), 0.0f);
 			} else {
-				glVertex3f(fX + kfInnerRadius*cos(fAngleStart)/1.9, fY + kfInnerRadius*sin(fAngleStart), 0.0);
-				glVertex3f(fX + kfRadius*cos(fAngleEnd)/1.9, fY + kfRadius*sin(fAngleEnd), 0.0);
+				glVertex3f(fX + kfInnerRadius*cosf(fAngleStart)/1.9f, fY + kfInnerRadius*sinf(fAngleStart), 0.0f);
+				glVertex3f(fX + kfRadius*cosf(fAngleEnd)/1.9f, fY + kfRadius*sinf(fAngleEnd), 0.0f);
 			}
 		}
 	glEnd();
@@ -49,10 +49,10 @@ void DrawStar(float fX, float fY) {
 void star(int base, int height,int x,int y){
 	glColor3f(0.0f, 0.0f, 1.0f); // Blue
 	for( int n = 0; n<5; n++){
-		float angleCos = cos(2*PI*n/5);
-		float angleSin =  sin(2*PI*n/5);
-		float xn1 = x+base * cos(2*PI*n/5);
-		float yn1 = y+base * sin(2*PI*n/5);
+		float angleCos = cosf(2.0f*M_PI*n/5.0f);
+		float angleSin =  sinf(2.0f*M_PI*n/5.0f);
+		float xn1 = x+base * cosf(2.0f*M_PI*n/5.0f);
+		float yn1 = y+base * sinf(2.0f*M_PI*n/5.0f);
 		eq_triangle(base,height,xn1,yn1,n);
 
 		
@@ -61,12 +61,12 @@ void star(int base, int height,int x,int y){
    
 }
 void rhombus(){
-   glColor3f(1,0.87,0);
+   glColor3f(1.0f,0.87f,0.0f);
    glBegin(GL_POLYGON);
-   glVertex2f(-0.83,0);
-   glVertex2f(0,0.76);
-   glVertex2f(0.83,0);
-   glVertex2f(0,-0.76);
+   glVertex2f(-0.83f,0.0f);
+   glVertex2f(0.0f,0.76f);
+   glVertex2f(0.83f,0.0f);
+   glVertex2f(0.0f,-0.76f);
    glEnd();
 
 }
@@ -75,7 +75,7 @@ void rhombus(){
 void draw(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	rhombus();
-	DrawStar(0.3,0.2);
+	DrawStar(0.3f,0.2f);
 	glFlush();	
     glutSwapBuffers();
 }
@@ -88,7 +88,7 @@ int main(int argc, char** argv){
    glutInitWindowPosition(100,100);
    glutCreateWindow("Brazil");
    glutDisplayFunc(draw);
-   glClearColor(0,0.60,0.22,0);
+   glClearColor(0.0f,0.60f,0.22f,0.0f);
    glutMainLoop();
    return 0;
 }
